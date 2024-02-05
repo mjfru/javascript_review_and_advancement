6e886a9a18a7a98a015458164f1d749e2742faec
Matt Frutchey <mjfru201@gmail.com>
2024-02-05 08:12:00 -0500

    amend notes for manipulating multiple elements

--- js_dom_interactions/02manipulate_dom.js
+++ js_dom_interactions/02manipulate_dom.js
@@ -48,14 +48,62 @@ h1.innerText += "<b>!!!!</b>"; // This will NOT work (unless it's your intention
  * Now let's look at elements that have properties/values that we can influence directly via JavaScript.
  */
 
-const inputs = document.querySelectorAll('input');
-inputs[0].value // "Would display what's set equal to value in the markup."
-inputs[0].value = '' // Might be used to reset a form entry after the input contents has been submitted and stored.
-inputs[2].checked // false (Assuming we had an unchecked checkbox)
+const inputs = document.querySelectorAll("input");
+inputs[0].value; // "Would display what's set equal to value in the markup."
+inputs[0].value = ""; // Might be used to reset a form entry after the input contents has been submitted and stored.
+inputs[2].checked; // false (Assuming we had an unchecked checkbox)
 
-const a = document.querySelector('a');
-a.href // Will give us the href value of <a>
-a.href = "http://www.google.com" // Element will now navigate you to Google!
+const a = document.querySelector("a");
+a.href; // Will give us the href value of <a>
+a.href = "http://www.google.com"; // Element will now navigate you to Google!
 
-const img = document.querySelector('img');
+const img = document.querySelector("img");
 img.src; // Will provide the src, the source, of the image.
+
+/*
+! getAttribute & setAttribute
+* They do, again, exactly what they sound like; get attributes and set new attributes for us.
+*/
+
+const range = document.querySelector('input[type="range"]'); // First input with the type of range
+range.getAttribute("max"); // takes the name of the attribute as a string
+range.getAttribute("min"); // likely 0
+range.getAttribute("type"); // range...pointless but enforces the point
+
+range.setAttribute("min", "-500"); // takes two arguments, the thing we're trying to set and the new value it will take.
+range.setAttribute("type", "radio"); // totally changes the type of the input
+
+/*
+! Methods to Find Parent, Children, and Sibling Elements
+* These methods allow you to find and work with other elements based on the one you're currently working with.
+*/
+
+const firstLi = document.querySelector("li"); // In something like this, we'll see that it'll have a parent element, ul.
+// So now, we have a way to indirectly access that ul element:
+firstLi.parentElement; // gives us <ul>..</ul>
+// We can go as many layers deep as exist:
+firstLi.parentElement.parentElement; // Likely returns body
+
+//? Let's work the other way now...
+const list = document.querySelector("ul"); // Now, let's access the children of this element
+list.children; // Lists the line items <li> in order.
+list.children[0]; // Gives us access to the first item in the list
+firstLi.nextElementSibling; // Will give us the next sibling, if any exist, in this list.
+firstLi.nextElementSibling.nextElementSibling; // Can also be chained
+
+const thirdLi = list.children[2];
+thirdLi.previousElementSibling; // Takes us to the...previous sibling in the list!
+
+/*
+! How can we use all this on multiple elements?
+* We'll use one of our methods to selector multiple things...such as getElementsByClassName, querySelectorAll, etc. and iterate over them.
+*/
+
+const allLis = document.querySelectorAll('li'); // All lis, saved to a variable
+for (let i = 0; i < allLis.length; i++) {
+  allLis[i].innerText = 'We did it!';   // All <li> elements now have this text!
+}
+
+for (let li of allLis) {  // A for...of...loop for variety.
+  li.innerHTML = 'We are <b>doing it</b>!' // This will be parsed and even the bold element will be enacted and displayed on the page.
+}
