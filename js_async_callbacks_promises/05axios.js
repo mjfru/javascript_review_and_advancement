4114c9b642f03ac641a1a2142ba0c9073ac03e1a
Matt Frutchey <mjfru201@gmail.com>
2024-02-12 08:07:07 -0500

    add axios notes

---
+++ js_async_callbacks_promises/05axios.js
@@ -0,0 +1,12 @@
+//! Axios - A Library for Making HTTP Requests
+/*
+The most popular of libraries that accomplish roughly the same thing.
+* It simplifies the fetch process and does a lot of the work behind the scenes and is promised-based, no nasty callbacks!
+* You can use it both in the client / browser side AND server side, making it versatile.
+? You can access Axios by either running:
+!     npm install axios 
+? OR including the following script in your HTML
+!     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+! If implementing the external script, it's imperative that it comes BEFORE your own JavaScript source link.
+* When we used fetch, we had to manually check the status code, with Axios, it knows that if it's a 404 or anything that's not a 200, it knows not to run and to reject it.
+*/
