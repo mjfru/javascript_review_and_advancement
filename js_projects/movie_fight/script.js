383cef0c81a8331934d37611bb4c835b2762843c
Matt Frutchey <mjfru201@gmail.com>
2024-02-17 12:03:48 -0500

    movie fight project v1 complete

--- js_projects/movie_fight/script.js
+++ js_projects/movie_fight/script.js
@@ -1,15 +1,10 @@
-// The searchTerm parameter comes from the event listener, taking that data and passing it in
-createAutoComplete({
-  root: document.querySelector(".autocomplete"),
+const autoCompleteConfig = {
   renderOption(movie) {
     const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
     return `
     <img src="${imgSrc}"/>
     ${movie.Title} (${movie.Year})
   `;
-  },
-  onOptionSelect(movie) {
-    onMovieSelect(movie);
   },
   inputValue(movie) {
     return movie.Title;
@@ -25,7 +20,7 @@ createAutoComplete({
         // s: 'avengers'
       },
     });
-  
+
     // Handling errors, such as no movie being found in the DB:
     if (response.data.Error) {
       return [];
@@ -33,11 +28,32 @@ createAutoComplete({
     // Uppercase 'S' because of what the API returns to us, this is nonstandard!
     return response.data.Search;
     // console.log(response.data);
-  }
+  },
+};
+
+// The searchTerm parameter comes from the event listener, taking that data and passing it in
+createAutoComplete({
+  ...autoCompleteConfig, // Make a copy of everything inside the object and throw them in here
+  root: document.querySelector("#left-autocomplete"),
+  onOptionSelect(movie) {
+    document.querySelector(".tutorial").classList.add("is-hidden");
+    onMovieSelect(movie, document.querySelector("#left-summary"), "left");
+  },
+});
+createAutoComplete({
+  ...autoCompleteConfig,
+  root: document.querySelector("#right-autocomplete"),
+  onOptionSelect(movie) {
+    document.querySelector(".tutorial").classList.add("is-hidden");
+    onMovieSelect(movie, document.querySelector("#right-summary"), "right");
+  },
 });
 
+let leftMovie;
+let rightMovie;
+
 //? Creating a helper function for selecting and displaying movies after choosing from the dropdown
-const onMovieSelect = async (movie) => {
+const onMovieSelect = async (movie, summaryElement, side) => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: "799bee64",
@@ -45,12 +61,65 @@ const onMovieSelect = async (movie) => {
       i: movie.imdbID,
     },
   });
-  document.querySelector("#summary").innerHTML = movieTemplate(response.data);
+  summaryElement.innerHTML = movieTemplate(response.data);
+  if (side === "left") {
+    leftMovie = response.data;
+  } else {
+    rightMovie = response.data;
+  }
+
+  if (leftMovie && rightMovie) {
+    runComparison();
+  }
   // console.log(reponse.data);
+};
+
+const runComparison = () => {
+  const leftSideStats = document.querySelectorAll(
+    "#left-summary .notification"
+  );
+  const rightSideStats = document.querySelectorAll(
+    "#right-summary .notification"
+  );
+
+  leftSideStats.forEach((leftStat, index) => {
+    const rightStat = rightSideStats[index];
+
+    const leftSideValue = parseInt(leftStat.dataset.value);
+    const rightSideValue = parseInt(rightStat.dataset.value);
+
+    if (rightSideValue > leftSideValue) {
+      leftStat.classList.remove("is-primary");
+      leftStat.classList.add("is-warning");
+    } else {
+      rightStat.classList.remove("is-primary");
+      rightStat.classList.add("is-warning");
+    }
+    console.log(leftStat, rightStat);
+  });
 };
 
 //? A helper function to create / populate HTML fields with information from a selected movie
 const movieTemplate = (movieDetail) => {
+  // Taking the Box Office details and turning them into an integer with $s or commas
+
+  const dollars = parseInt(
+    movieDetail.BoxOffice.replace(/\$/g, "").replace(/,/g, "")
+  );
+  const metascore = parseInt(movieDetail.Metascore);
+  const imdbRating = parseFloat(movieDetail.imdbRating);
+  const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ""));
+  // console.log(dollars, metascore, imdbRating, imdbVotes);
+  const awards = movieDetail.Awards.split(" ").reduce((prev, word) => {
+    const value = parseInt(word);
+    if (isNaN(value)) {
+      return prev;
+    } else {
+      return prev + value;
+    }
+  }, 0);
+  console.log(awards);
+
   return `
     <article class="media">
       <figure class="media-left">
@@ -66,23 +135,23 @@ const movieTemplate = (movieDetail) => {
         </div>
       </div>
     </article>
-    <article class="notification is-primary>
+    <article data-value=${awards} class="notification is-primary">
       <p class="title">${movieDetail.Awards}</p>
       <p class="subtitle">Awards:</p>
     </article>
-    <article class="notification is-primary>
+    <article data-value=${dollars} class="notification is-primary">
       <p class="title">${movieDetail.BoxOffice}</p>
       <p class="subtitle">Box Office:</p>
     </article>
-    <article class="notification is-primary>
+    <article data-value=${metascore} class="notification is-primary">
       <p class="title">${movieDetail.Metascore}</p>
       <p class="subtitle">Metascore:</p>
     </article>
-    <article class="notification is-primary>
+    <article data-value=${imdbRating} class="notification is-primary">
       <p class="title">${movieDetail.imdbRating}</p>
       <p class="subtitle">IMDB Rating:</p>
     </article>
-    <article class="notification is-primary>
+    <article data-value=${imdbVotes} class="notification is-primary">
       <p class="title">${movieDetail.imdbVotes}</p>
       <p class="subtitle">IMDB Votes:</p>
     </article>
