d67010851fe59d499ee3553973ce6b0b1436caf0
Matt Frutchey <mjfru201@gmail.com>
2024-02-22 12:50:26 -0500

    mvp complete, 'nls' - node ls, shows files and folders in an array

--- js_projects/node_js/node_project/index.js
+++ js_projects/node_js/node_project/index.js
@@ -1,15 +1,17 @@
-const fs = require("fs"); // fs = filesystem
+#!/usr/bin/env node
+const fs = require('fs'); // fs = filesystem
 
 // What's going to go in here? - the path[, options], callback function
 // '.' indicates the current directory
+//? process.cwd() offers better cross-compatability, so let's use that instead though to see our current working directory
 // The first possible argument that the callback function could get is an error object, often shortened to err.
-fs.readdir(".", (err, filenames) => {
+fs.readdir(process.cwd(), (err, filenames) => {
   // Either err === to an error object
   if (err) {
     // Error handling
     console.log(err);
     // throw new Error(err);
   }
-  console.log(filenames);
   // Or, err === null (all is OK)
+  
 });
