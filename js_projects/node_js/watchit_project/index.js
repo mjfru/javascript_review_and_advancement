8fbd60a8aed7bc010988c744fce565a69ba707ae
Matt Frutchey <mjfru201@gmail.com>
2024-02-23 13:29:26 -0500

    add a debounce functioin from lodash

--- js_projects/node_js/watchit_project/index.js
+++ js_projects/node_js/watchit_project/index.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-console.log('I worked!');
+const debounce = require('lodash.debounce');
+const chokidar = require('chokidar');
+
+const start = debounce(() => {
+  console.log('Starting program');
+}, 100);
+
+chokidar
+  .watch('.')
+  .on('add', start)
+  .on('change', () => console.log('File Changed'))
+  .on('unlink', () => console.log('File Unlinked'));
