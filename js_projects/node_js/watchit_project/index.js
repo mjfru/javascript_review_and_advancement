6f55765de8e559862a95fe564255eeb6688a3ff4
Matt Frutchey <mjfru201@gmail.com>
2024-02-23 13:58:02 -0500

    general refactor

--- js_projects/node_js/watchit_project/index.js
+++ js_projects/node_js/watchit_project/index.js
@@ -2,13 +2,31 @@
 
 const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
+//? Naming it 'program' to be more conventional that will be used over and over.
+const program = require('caporal');
+const fs = require('fs');
 
-const start = debounce(() => {
-  console.log('Starting program');
-}, 100);
+program
+  .version('1.0.0')
+  .argument('[filename]', 'Name of file to execute')
+  .action(async ({ filename }) => {
+    const name = filename || 'index.js';
+    try {
+      await fs.promises.access(name);
+    } catch (err) {
+      throw new Error(`Could not find the file ${name}`)
+    }
 
-chokidar
-  .watch('.')
-  .on('add', start)
-  .on('change', () => console.log('File Changed'))
-  .on('unlink', () => console.log('File Unlinked'));
+    const start = debounce(() => {
+      console.log('Starting program');
+    }, 100);
+    
+    chokidar
+      .watch('.')
+      .on('add', start)
+      .on('change', start)
+      .on('unlink', start);
+  });
+
+program.parse(process.argv);
+
