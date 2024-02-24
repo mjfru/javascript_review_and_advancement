fd8470065daa8c09571508ac32c49f7f91e32203
Matt Frutchey <mjfru201@gmail.com>
2024-02-24 08:54:07 -0500

    final refactor complete

--- js_projects/node_js/watchit_project/index.js
+++ js_projects/node_js/watchit_project/index.js
@@ -5,6 +5,8 @@ const chokidar = require('chokidar');
 //? Naming it 'program' to be more conventional that will be used over and over.
 const program = require('caporal');
 const fs = require('fs');
+const { spawn } = require('child_process');
+// const chalk = require('chalk');
 
 program
   .version('1.0.0')
@@ -17,8 +19,14 @@ program
       throw new Error(`Could not find the file ${name}`)
     }
 
+    let proc;
     const start = debounce(() => {
-      console.log('Starting program');
+      if (proc) {
+        proc.kill();
+      }
+      console.log('>>> Starting process...')
+      // console.log(chalk.blue('>>> Starting process...'));
+      proc = spawn('node', [name], { stdio: 'inherit' });
     }, 100);
     
     chokidar
