c858a498222ce6f1c96a5d9f53454e673f3e8043
Matt Frutchey <mjfru201@gmail.com>
2024-02-24 10:34:34 -0500

    initialize simple form for admin signup

--- js_projects/ecommerce_project/index.js
+++ js_projects/ecommerce_project/index.js
@@ -1,1 +1,28 @@
-console.log("Project is starting!")
+const express = require('express');
+
+// Object that describes everything the app can do using express
+const app = express();
+
+// Route Handler:
+// Request from a browser to the server, Response, from the server to browser
+app.get('/', (req, res) => {
+  res.send(
+  `
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input name="passwordConfirm" placeholder="password confirmation" />
+        <button>Sign Up</button>
+      </form>
+    </div>
+  `);
+});
+
+app.post('/', (req, res) => {
+  res.send('Account Created!');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on Port 3000.')
+})
