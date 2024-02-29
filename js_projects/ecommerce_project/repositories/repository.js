11daf40b4be75e7a6b6afc8406bda1ab5586b12a
Matt Frutchey <mjfru201@gmail.com>
2024-02-29 06:48:12 -0500

    add new and extend repository pages, create base product form

---
+++ js_projects/ecommerce_project/repositories/repository.js
@@ -0,0 +1,89 @@
+const fs = require("fs");
+const crypto = require("crypto");
+
+module.exports = class Repository {
+  constructor(filename) {
+    if (!filename) {
+      throw new Error("Creating a repository requires a filename.");
+    }
+    this.filename = filename;
+    try {
+      fs.accessSync(this.filename);
+    } catch (err) {
+      fs.writeFileSync(this.filename, "[]");
+    }
+  }
+
+  async create(attributes) {
+    attributes.id = this.randomId();
+    const records = await this.getAll();
+    records.push(attributes);
+    await this.writeAll(records);
+
+    return attributes;
+  }
+
+  async getAll() {
+    // Open the file called this.filename
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, { encoding: "utf8" })
+    );
+  }
+
+  // Write All
+  async writeAll(records) {
+    await fs.promises.writeFile(
+      this.filename,
+      JSON.stringify(records, null, 2)
+    );
+  }
+
+  // Generating and assigning a random ID:
+  randomId() {
+    return crypto.randomBytes(4).toString("hex");
+  }
+
+  // Get One:
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find((record) => record.id === id);
+  }
+
+  // Delete One:
+  async delete(id) {
+    const records = await this.getAll();
+    const filteredRecords = records.filter((record) => record.id !== id);
+    await this.writeAll(filteredRecords);
+  }
+
+  // Update
+  async update(id, attributes) {
+    const records = await this.getAll();
+    const record = records.find((record) => record.id === id);
+    if (!record) {
+      throw new Error(`Record with id ${id} not found!`);
+    }
+    // record === {email: 'test@test.com'} attributes === {password: newpassword}
+    Object.assign(record, attributes);
+    // record == { email: 'test@test.com', password: 'newpassword' }
+    await this.writeAll(records);
+  }
+
+  // Get One:
+  async getOneBy(filters) {
+    const records = await this.getAll();
+    // Iterating through an array
+    for (let record of records) {
+      let found = true;
+      // 'In' indicating we're iterating through an object
+      for (let key in filters) {
+        if (record[key] !== filters[key]) {
+          found = false;
+        }
+      }
+      if (found) {
+        return record;
+      }
+    }
+  }
+};
--- js_projects/ecommerce_project/repositories/users.js
+++ js_projects/ecommerce_project/repositories/users.js
@@ -1,120 +1,46 @@
 const fs = require("fs");
 const crypto = require("crypto");
 const util = require("util");
+const Repository = require('./repository');
 const scrypt = util.promisify(crypto.scrypt);
 
-class UsersRepository {
+// Make the Repository methods available in UsersRepository:
+class UsersRepository extends Repository {
   // Check and see if there's already a file created to store information:
-  constructor(filename) {
-    if (!filename) {
-      throw new Error("Creating a repository requires a filename.");
+    // Create
+    async create(attributes) {
+      // Remember, attributes has email: 'string', password: 'string'
+      attributes.id = this.randomId();
+  
+      // A random series of numbers and letters for our salt:
+      const salt = crypto.randomBytes(8).toString("hex");
+      
+      // Creating the hash:
+      const buf = await scrypt(attributes.password, salt, 64);
+  
+      const records = await this.getAll(); // Getting all the users array
+      // Add the new user to the array
+      const record = {
+        // Take all the properties from attributes
+        ...attributes,
+        // But replace the default password with this:
+        password: `${buf.toString("hex")}.${salt}`,
+      };
+      records.push(record);
+  
+      await this.writeAll(records);
+      return record;
     }
-    this.filename = filename;
-    try {
-      fs.accessSync(this.filename);
-    } catch (err) {
-      fs.writeFileSync(this.filename, "[]");
+  
+    // Compare passwords:
+    async comparePasswords(saved, supplied) {
+      // Saved = password saved in our DB. 'hashed.salt'
+      // Supplied = password given to us by a user.
+      const [hashed, salt] = saved.split('.');
+      const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
+
+      return hashed === hashedSuppliedBuf.toString('hex');
     }
-  }
-  async getAll() {
-    // Open the file called this.filename
-    return JSON.parse(
-      await fs.promises.readFile(this.filename, { encoding: "utf8" })
-    );
-  }
-
-  // Create
-  async create(attributes) {
-    // Remember, attributes has email: 'string', password: 'string'
-    attributes.id = this.randomId();
-
-    // A random series of numbers and letters for our salt:
-    const salt = crypto.randomBytes(8).toString("hex");
-    
-    // Creating the hash:
-    const buf = await scrypt(attributes.password, salt, 64);
-
-    const records = await this.getAll(); // Getting all the users array
-    // Add the new user to the array
-    const record = {
-      // Take all the properties from attributes
-      ...attributes,
-      // But replace the default password with this:
-      password: `${buf.toString("hex")}.${salt}`,
-    }
-
-    records.push(record);
-
-    await this.writeAll(records);
-    return record;
-  }
-
-  // Compare passwords:
-  async comparePasswords(saved, supplied) {
-    // Saved = password saved in our DB. 'hashed.salt'
-    // Supplied = password given to us by a user.
-    const [hashed, salt] = saved.split('.');
-    const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
-
-    return hashed === hashedSuppliedBuf.toString('hex');
-  }
-
-  // Write All
-  async writeAll(records) {
-    await fs.promises.writeFile(
-      this.filename,
-      JSON.stringify(records, null, 2)
-    );
-  }
-
-  // Generating and assigning a random ID:
-  randomId() {
-    return crypto.randomBytes(4).toString("hex");
-  }
-
-  // Get One:
-  async getOne(id) {
-    const records = await this.getAll();
-    return records.find(record => record.id === id);
-  }
-
-  // Delete One:
-  async delete(id) {
-    const records = await this.getAll();
-    const filteredRecords = records.filter(record => record.id !== id);
-    await this.writeAll(filteredRecords);
-  }
-
-  // Update
-  async update(id, attributes) {
-    const records = await this.getAll();
-    const record = records.find(record => record.id === id);
-    if (!record) {
-      throw new Error(`Record with id ${id} not found!`);
-    }
-    // record === {email: 'test@test.com'} attributes === {password: newpassword}
-    Object.assign(record, attributes);
-    // record == { email: 'test@test.com', password: 'newpassword' }
-    await this.writeAll(records);
-  }
-
-  // Get One:
-  async getOneBy(filters) {
-    const records = await this.getAll();
-    // Iterating through an array
-    for (let record of records) {
-      let found = true;
-      // 'In' indicating we're iterating through an object
-      for (let key in filters) {
-        if (record[key] !== filters[key]) {
-          found = false;
-        }
-      }
-      if (found) {
-        return record;
-      }
-    }
-  }
 }
 
 module.exports = new UsersRepository("users.json");
