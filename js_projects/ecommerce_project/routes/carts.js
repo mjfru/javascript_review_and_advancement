7bac9d9da672b3fe0c7a743e1d867567af73f218
Matt Frutchey <mjfru201@gmail.com>
2024-03-03 21:09:01 -0500

    test method to add item to a unique user's cart

--- js_projects/ecommerce_project/routes/carts.js
+++ js_projects/ecommerce_project/routes/carts.js
@@ -1,13 +1,26 @@
-const express = require('express');
+const express = require("express");
 
+const cartsRepo = require("../repositories/carts");
 const router = express.Router();
 
 // Receive a POST request to add an item to a cart
-router.post('/cart/products', (req, res) => {
-  console.log(req.body.productId);
+router.post("/cart/products", async (req, res) => {
+  // console.log(req.body.productId);
+  let cart;
+  if (!req.session.cartId) {
+    // We don't have a cart, need to create one and store the cart ID on req.session.cartId property
+    cart = await cartsRepo.create({ items: [] });
+    req.session.cartId = cart.id;
+  } else {
+    // Get it from the repository
+    cart = await cartsRepo.getOne(req.session.cartId);
+  }
+  console.log(cart);
+  // Either increment quantity for existing product
 
-  res.send('Product added to cart!');
-})
+  // Or add new product to items array
+  res.send("Product added to cart!");
+});
 
 // Receive a GET request to show all items in the cart
 
