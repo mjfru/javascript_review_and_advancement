69d12b67c7fd944b02301f0cd2031bf47f9ee677
Matt Frutchey <mjfru201@gmail.com>
2024-03-05 09:09:41 -0500

    add brief notes regarding testing and the goals for this section

---
+++ js_testing/01testing_overview.js
@@ -0,0 +1,15 @@
+//! Goals of Testing
+/*
+
+* 1. Automate the process of ensuring that our app works as we expect.
+
+* 2. Keep us from having to manually click around the app to find bugs (manual testing).
+
+* 3. Make sure the app still works as expected even after we change something.
+
+? Instructor's thoughts:
+Generally, people spend more time - setting up - testing than they do writing tests.
+All testing frameworks / libraries are essentially the same.
+Technically, you don't even have to use a library!
+
+*/
